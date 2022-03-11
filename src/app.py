cf05d95086cc0d651d92c8fb5dc8b7a1f6b90228
jack <jack.7e@protonmail.com>
2022-03-11 20:41:10 +0000

    api returns nfts

--- src/app.py
+++ src/app.py
@@ -1,12 +1,16 @@
 import os
+import base64
 import random
 
 from flask import Flask, jsonify, request
 from Bitbots import Bitbots
 
+from flask_cors import CORS
+
 b = Bitbots()
 nfts = b.shuffle()
 app = Flask(__name__)
+CORS(app)
 app.config["SERVER_NAME"] = "127.0.0.1:5000"
 
 
@@ -24,16 +28,21 @@ def home():
 @app.route("/generate")
 def generate():
     b.generate()
+    return "Done"
     # add post method 
 
 
-# get specific
-@app.route("/nft")
-def nft():
+@app.route("/rand")
+def random_nft():
     n = random.randint(0, 8192)
-    return nfts[str(n)]
+    print("sending...")
+    return jsonify(nfts[str(n)])
 
+# get specific
+@app.route("/nfts")
+def nft():
+    # warning can be a 300 mb call
+    return jsonify(nfts)
 
 # TODO show svg image of nft
-
 # TODO request purchase
