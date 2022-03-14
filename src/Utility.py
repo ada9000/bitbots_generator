902ecf35c4ec47aa3c5efb342d4e33fce9ee684f
jack-0 <github.ay8yf@slmail.me>
2022-03-14 23:34:19 +0000

    added Utility.py

---
+++ src/Utility.py
@@ -0,0 +1,32 @@
+import json
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+# consts ---------------------------------------------------------------------
+# logging colours
+COLOR_RESET = '\033[0m'
+COLOR_CYAN = '\033[0;36m'
+COLOR_YELLOW = '\033[1;33m'
+COLOR_RED = '\033[1;31m'
+
+# functions ------------------------------------------------------------------
+def log_debug(msg:str):
+    msg = " " + COLOR_YELLOW + msg + COLOR_RESET
+    logging.debug(msg)
+    pass
+
+def log_info(msg:str):
+    msg = " " + COLOR_CYAN + msg + COLOR_RESET
+    logging.info(msg)
+
+def read_file_return_data(filepath):
+    data = {}
+    with open(filepath) as f:
+        data = json.load(f)
+    return data
+ 
+def write_json(filepath, data):
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    return
+
