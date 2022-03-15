9f5695cdc5fc5b71968fe21e890b83139e6f3abd
jack-0 <jack.7e@protonmail.com>
2022-03-15 20:55:23 +0000

    fixed string format :bug:

--- src/CardanoComms.py
+++ src/CardanoComms.py
@@ -9,6 +9,7 @@ import time
 
 
 from Bitbots import *
+import re
 
 #-----------------------------------------------------------------------------
 MAINNET = "--mainnet"
@@ -36,7 +37,15 @@ def cmd_out(cmd):
 def replace_b_str(msg):
     """ remove 'b' token and single quote from a str """
     msg = str(msg)
-    msg = msg.replace('b\'','')
+    # use regex to match all text between b'<target>'
+    re_res = re.findall(r'b\'(.*?)\'', msg)
+    for i, s in enumerate(re_res):
+        if i == 0:
+            msg = s
+        else:
+            msg += ','
+            msg += s
+    # replace any of the following symbols
     msg = msg.replace('\'','')
     msg = msg.replace('[','')
     msg = msg.replace(']','')
@@ -70,6 +79,9 @@ class Wallet:
         self.addr_path  = WALLET_DIR + sub_dir + 'base.addr'
         self.skey       = WALLET_DIR + sub_dir + 'payment.skey'
         self.vkey       = WALLET_DIR + sub_dir + 'payment.vkey'
+        # tx files are stored in a wallet
+        self.tx_raw     = WALLET_DIR + sub_dir + 'tx.raw'
+        self.tx_signed  = WALLET_DIR + sub_dir + 'tx.signed'
         # check all required files exist
         files = [self.addr_path, self.skey, self.vkey]
         for f in files:
@@ -100,9 +112,9 @@ class Wallet:
             # get utxo from utxos
             utxo = utxos[tx_idx].split()
             # remove unwanted characters, cardano-cli is strict on whitespace
-            utxo = replace_b_str(utxo)
-            utxo = utxo.replace(' ','')
             # convert back into an array
+            utxo = str(utxo)
+            utxo = replace_b_str(utxo)
             utxo = utxo.split(',')
             # check for utxos that contain nfts by looking for
             # multiple tokens past the '+' token
@@ -171,22 +183,28 @@ class CardanoComms:
             self.clean()
         # generate policy keys, check for err
         log_debug("creating policy keys")
-        cmd = "cardano-cli address key-gen --verification-key-file " + self.policy_vkey + " --signing-key-file " + self.policy_skey
+        cmd = "cardano-cli address key-gen"+\
+            " --verification-key-file " + self.policy_vkey+\
+            " --signing-key-file " + self.policy_skey
         res = cmd_out(cmd)
         if str(res) != EMPTY_BYTE_STRING:
             raise Exception("Policy Key creation error", res)
         # get protocol json, check for err
         log_debug("fetching procol json")
-        cmd ="cardano-cli query protocol-parameters " + self.network + " --out-file " + self.protocol_path 
+        cmd ="cardano-cli query protocol-parameters " + self.network +\
+            " --out-file " + self.protocol_path 
         res = cmd_out(cmd)
         if str(res) != EMPTY_BYTE_STRING:
             raise Exception("Protocol json creation error", res)
         # generate key hash, check for errors
         log_debug("generating key hash")
-        cmd = "cardano-cli address key-hash --payment-verification-key-file " + self.policy_vkey
+        cmd = "cardano-cli address key-hash"+\
+            " --payment-verification-key-file " + self.policy_vkey
         res = cmd_out(cmd)
         # stip unwanted chars from key_hash   
-        self.key_hash = str(res).replace('b\'','').replace('\\n\'','')
+        self.key_hash = replace_b_str(res)#str(res).replace('b\'','').replace('\\n\'','')
+        print(self.key_hash)
+        breakpoint()
         # check key hash is correct
         if len(self.key_hash) != 56:
             raise Exception("Generate Key Hash Error", self.keyhash)
@@ -197,7 +215,9 @@ class CardanoComms:
         # multiple expire time by 3600 seconds and add to amend inputed hours to the target slot
         self.target_slot = current_slot + 8760 * 3600
         self.write_json(self.slot_path, {"slot":self.target_slot})
-        log_debug("slot: current=" + str(current_slot) + " target=" + str(self.target_slot) + " diff="+str((self.target_slot - current_slot)))
+        log_debug("slot current : " + str(current_slot))
+        log_debug("slot target  : " + str(self.target_slot))
+        log_debug("slot diff    : " + str((self.target_slot - current_slot)))
         # populate the policy script template and write as json
         policy_script = {
             "type": "all",
@@ -214,7 +234,8 @@ class CardanoComms:
         write_json(self.policy_script, policy_script)
         # gen policy id and clean result
         log_debug("generating policy id")
-        cmd = "cardano-cli transaction policyid --script-file " + self.policy_script
+        cmd = "cardano-cli transaction policyid"+\
+            " --script-file " + self.policy_script
         res = cmd_out(cmd)
         self.policy_id = str(res).replace('b\'','').replace('\\n\'','')
         write_json(self.policy_id_path, {"id":self.policy_id})
@@ -239,8 +260,10 @@ class CardanoComms:
             nft_id = i
         # return meta
         return nft_id, metadata
-
-    def mint(self, metadata_path:str, recv_addr:str, mint_wallet:Wallet):
+    
+    
+    # nft mint ---------------------------------------------------------------
+    def mint_nft(self, metadata_path:str, recv_addr:str, mint_wallet:Wallet):
         # read meta and 'fix' (insert policy id into it)
         metadata = read_file_return_data(metadata_path)
         nft_id, metadata = self.add_policy_id_to_meta(metadata)
@@ -249,9 +272,20 @@ class CardanoComms:
         change = 0 # 1.5 ada
         min_mint_cost = 1500000
         # template build
-        self.build_tx(fee="0", change=change, recv_addr=recv_addr, mint_wallet=mint_wallet, nft_id=nft_id, min_mint_cost=min_mint_cost, metadata_path=metadata_path)
+        self.build_mint_tx(fee="0", change=change,
+            recv_addr=recv_addr,
+            mint_wallet=mint_wallet,
+            nft_id=nft_id,
+            min_mint_cost=min_mint_cost,
+            metadata_path=metadata_path)
         witness = "1"
-        cmd = "cardano-cli transaction calculate-min-fee --tx-body-file matx.raw --tx-in-count 1 --tx-out-count 1 --witness-count " + witness + " " + self.network + " --protocol-params-file " + self.protocol_path + " | cut -d \" \" -f1"
+        cmd = "cardano-cli transaction calculate-min-fee"+\
+            " --tx-body-file " + mint_wallet.tx_raw+\
+            " --tx-in-count 1 --tx-out-count 1"+\
+            " --witness-count " + witness +\
+            " " + self.network +\
+            " --protocol-params-file " + self.protocol_path+\
+            " | cut -d \" \" -f1"
         fee = cmd_out(cmd)
         fee = str(fee).replace('b\'','').replace('\\n\'','')
         # get funds
@@ -259,20 +293,37 @@ class CardanoComms:
         funds = mint_wallet.lace
         # calculate change
         change = int(funds) - int(fee) - int(min_mint_cost)
-        log_debug("fee="+str(funds))
-        log_debug("funds="+str(funds))
-        log_debug("min-mint-cost=" + str(min_mint_cost))
-        log_debug("funds  ="+str(funds))
-        log_debug("change =" + str(change))
+        log_debug("fee      : " +str(fee))
+        log_debug("min-mint : " + str(min_mint_cost))
+        log_debug("funds    : " +str(funds))
+        log_debug("change   : " + str(change))
+        log_debug("diff lace: " + str(int(funds) - int(change)))
+        log_debug("diff ada : " + str( (int(funds) - int(change)) / 1000000 ))
+        # check if we have enough funding
+        min_funds_required = int(min_mint_cost) + int(fee) + 10
+        if funds < min_funds_required:
+            log_error("Aborting mint."+\
+                " Low funds " + str(funds)+\
+                " expected " + str(min_funds_required))
+            return False
         #build
-        self.build_tx(fee=fee, change=change, recv_addr=recv_addr, mint_wallet=mint_wallet, nft_id=nft_id, min_mint_cost=min_mint_cost, metadata_path=metadata_path)
+        self.build_mint_tx(fee=fee,
+            change=change,
+            recv_addr=recv_addr,
+            mint_wallet=mint_wallet,
+            nft_id=nft_id,
+            min_mint_cost=min_mint_cost,
+            metadata_path=metadata_path)
         #sign
-        self.sign_tx(mint_wallet=mint_wallet)
+        if self.sign_mint_tx(wallet=mint_wallet) is False:
+            return False
         # send
-        self.submit_tx(recv_addr)
+        self.submit_tx(recv_addr=recv_addr, wallet=mint_wallet)
+        return True
 
 
-    def build_tx(self, fee, change, recv_addr, mint_wallet, nft_id, min_mint_cost, metadata_path):
+    def build_mint_tx(self, fee, change, recv_addr, mint_wallet, nft_id, min_mint_cost, metadata_path):
+        """ builds a nft transaction """
         # build nft mint str (assume amount to be 1)
         nft_mint_str = "\""
         nft_mint_str = nft_mint_str + "1" + " " + self.policy_id + "." + nft_id
@@ -292,46 +343,51 @@ class CardanoComms:
         # set the mint wallet as our change address
         tx_out = " --tx-out " + mint_wallet.addr + "+" + change
         # template transaction for cmd string
-        build_raw = "cardano-cli transaction build-raw " +\
-        " --fee "+ fee +\
-        " --tx-out " + recv_addr + "+" + min_mint_cost + "+" + nft_mint_str +\
-        tx_out +\
-        tx_in + \
-        " --mint=" + nft_mint_str +\
-        " --minting-script-file " + self.policy_script +\
-        " --metadata-json-file " + metadata_path +\
-        " --invalid-hereafter " + str(self.target_slot) +\
-        " --out-file matx.raw"
+        build_raw = "cardano-cli transaction build-raw "+\
+            " --fee "+ fee +\
+            " --tx-out " + recv_addr + "+" + min_mint_cost + "+" + nft_mint_str+\
+            tx_out +\
+            tx_in +\
+            " --mint=" + nft_mint_str +\
+            " --minting-script-file " + self.policy_script+\
+            " --metadata-json-file " + metadata_path +\
+            " --invalid-hereafter " + str(self.target_slot)+\
+            " --out-file " + mint_wallet.tx_raw
         # remove any double whitespace
         build_raw = build_raw.replace("  "," ")
+        log_debug(build_raw)
         # run build tx cmd
         res = cmd_out(build_raw)
         if str(res) == EMPTY_BYTE_STRING:
             log_info("build tx success")
+        else:
+            res = replace_b_str(res)
+            log_error(str(res))
         return
 
 
-    def sign_tx(self, mint_wallet:Wallet):
+    def sign_mint_tx(self, wallet:Wallet):
         sign_tx = "cardano-cli transaction sign" + \
-        " --signing-key-file " + mint_wallet.skey + \
-        " --signing-key-file " + self.policy_skey +\
-        " "+ self.network +\
-        " --tx-body-file matx.raw"+\
-        " --out-file matx.signed"
+            " --signing-key-file " + wallet.skey + \
+            " --signing-key-file " + self.policy_skey +\
+            " "+ self.network +\
+            " --tx-body-file " + wallet.tx_raw+\
+            " --out-file " + wallet.tx_signed
         # 
         res = cmd_out(sign_tx)
         if str(res) != EMPTY_BYTE_STRING:
-            log_debug("signing failed! " + str(res))
-            return
+            log_error("signing failed! " + str(res))
+            return False
         log_info("signing success")
-        return
+        return True
 
 
-    def submit_tx(self, recv_addr):
-        cmd = "cardano-cli transaction submit --tx-file matx.signed " + self.network
+    def submit_tx(self, recv_addr:str, wallet:Wallet):
+        cmd = "cardano-cli transaction submit"+\
+            " --tx-file " + wallet.tx_signed + " " + self.network
         res = cmd_out(cmd)
         res = replace_b_str(res)
-        res.replace('\n','')
+        res = res.replace('\n','')
         log_info(res)
         transaction_sent = False
         idx = 0
@@ -348,6 +404,8 @@ class CardanoComms:
                 idx += 1
         return
 
+# Refund manger class
+# Mint manger class
 
 if __name__ == "__main__":
     print("Running tests")
@@ -359,4 +417,4 @@ if __name__ == "__main__":
     wallet = Wallet()
     wallet.update_utxos()
     cc = CardanoComms(TESTNET, False)
-    cc.mint(metadata_path="../output/0009.json", recv_addr=wallet.addr, mint_wallet=wallet)
+    cc.mint_nft(metadata_path="../output/0013.json", recv_addr=wallet.addr, mint_wallet=wallet)
