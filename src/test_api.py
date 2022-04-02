4ed868da5626ecf0242aa510690eee02ed82c937
jack-0 <jack.7e@protonmail.com>
2022-04-02 09:26:39 +0000

    added simple api test

---
+++ src/test_api.py
@@ -0,0 +1,18 @@
+from CardanoComms import *
+import time
+from multiprocessing import Process
+
+PRICE = 5
+NFT_LEN = 60
+
+
+def test_api(i):
+    res = cmd_out("curl 192.168.1.95:5000/mint_price")
+
+
+if __name__ == "__main__":
+    for i in range(1000):
+        print(i)
+        p = Process(target=test_api, args=(i,))
+        p.start()
+        #time.sleep(0.2)
