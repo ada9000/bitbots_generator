c24b49e799cea7116b7c2e2e03bd5bf0809a17e0
jack-0 <jack.7e@protonmail.com>
2022-04-03 10:14:42 +0000

    updated NFT to include version

--- src/Nft.py
+++ src/Nft.py
@@ -5,42 +5,40 @@ class Nft:
     def __init__(self, policyid:str="todo"):
 
         self.policyid = policyid
+        self.nft_name = ''
         self.nft_CIP = '721'
         self.new_CIP = NEW_CIP
+        self.version = '2.0'
         # mime: data:image/svg+xml;utf8
 
     def generate_nft(self, nft_name:str, payload_ref:int, nft_payload:list, nft_references:list, properties):
         # TODO note nft_references might be ints but json only allows string keys
         meta = {}
+        self.nft_name = nft_name
         # 721 
         # notice the new CIP line
         # TODO convert this to variables passed into method
         nft_details = {
-            'project':'Copyright Bitbots.art 2022',
+            'project':'Bitbots',
+            'name':nft_name,
             'traits':properties,
-            'description':'nft showcasing new CIP',
+            #'type':'NPC', TODO
             self.new_CIP: {
                 'mediaType':'image/svg+xml;utf8,','ref':nft_references
                 }
             }
-        #meta[self.nft_CIP] = {self.policyid:{nft_name:nft_details}}
 
         if nft_payload != None:
-            # TODO add multiple payloads i.e
-            # '1':data,
-            # '2':data,
-            # 'n':data
-
             # include new CIP payload
             meta = {
                 self.nft_CIP:{
                     self.policyid:{
                         nft_name:nft_details
-                        }
+                        },
                     }, 
+                    'version':self.version,
                 self.new_CIP:{
                     self.policyid:{
-                        "NOTE":"COPYRIGHT Bitbots.art 2022",
                         str(payload_ref):nft_payload
                         }
                     }
@@ -52,12 +50,21 @@ class Nft:
                     self.policyid:{
                         nft_name:nft_details
                         }
-                    }
+                    },
+                    'version':self.version,
                 }
-        
         return meta
+
 
     def append_more_data(self, meta, payload_ref:int, nft_payload:list):
         new_meta = copy.deepcopy(meta)
         new_meta[self.new_CIP][self.policyid][payload_ref] = nft_payload
+        return new_meta
+
+
+    def change_type(self, meta, type):
+        if self.nft_name == '':
+            raise Exception("Invalid nft name")
+        new_meta = copy.deepcopy(meta)
+        new_meta[self.nft_CIP][self.policyid][self.nft_name]['type'] = type
         return new_meta
