cfc52d76f91191c2d78d34b1ac543a7cb2d989bf
jack-0 <github.ay8yf@slmail.me>
2022-06-05 22:01:46 +0000

    ipfs operation is now pinned

--- src/Ipfs.py
+++ src/Ipfs.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from blockfrost import BlockFrostIPFS, ApiError
-import os
+import os, time
 from Utility import *
 
 class IpfsManager:
@@ -15,38 +15,71 @@ class IpfsManager:
             project_id=self.apiKey
         )
 
+    
     def add(self, file_path: str):
         file_hash = None
-        file_data = None
-        ipfs_object = None
-
         error = False
-
         try:
+            # get hash
             ipfs_object = self.ipfs.add(file_path)
             file_hash = ipfs_object.ipfs_hash
-            #print(file_hash)
+            print(file_hash)
         except ApiError as e:
             error = True
             log_error(str(e))
 
         try:
-            file_data = self.ipfs.gateway(IPFS_path=file_hash).text
-            #print(file_data)
+            # pin
+            res = self.ipfs.pin_object(file_hash)
+            file_hash = res.ipfs_hash
+            status = res.state
         except ApiError as e:
             error = True
             log_error(str(e))
-
-        # TODO PIN!
-
-
+        
         if error:
-            log_error("IPFS error hit")
-            breakpoint()
-            self.add()
-        else:
-            # set success in db 
-            # add parameters
-            pass
+            log_debug(f"IPFS: Issue adding and pinning '{file_hash}' generated from '{file_path}'")
+            breakpoint() # remove or leave?
+            time.sleep(0.2)
+            self.add(file_path)
 
         return file_hash
+
+    
+    def remove(self, file_hash: str):
+        # REMOVE TODO delete after testing
+        try:
+            res = self.ipfs.pined_object_remove(file_hash)
+            file_hash = res.ipfs_hash
+            status = res.state
+            breakpoint()
+        except ApiError as e:
+            log_error(str(e))
+            breakpoint()
+        return
+
+
+    def get(self, file_hash):
+        try:
+            file_data = self.ipfs.gateway(IPFS_path=file_hash).text
+        except ApiError as e:
+            log_error(str(e))
+            breakpoint()
+        return file_data
+
+
+    def get_all_pinned(self):
+        try:
+            pinned = self.ipfs.pined_list()
+        except ApiError as e:
+            log_error(str(e))
+        return pinned
+
+
+    def remove_all_pinned_TEST_ONLY(self):
+        pinned = self.get_all_pinned()
+        for i in pinned:
+            time.sleep(0.02)
+            self.remove(i.ipfs_hash)
+
+
