655d1f8a568904cf51de8af1143d5b295baf7ddc
jack-0 <github.ay8yf@slmail.me>
2022-06-11 16:38:48 +0000

    added script to test set

---
+++ src/TestSet.py
@@ -0,0 +1,26 @@
+import os
+from ApiManager import *
+from dotenv import load_dotenv
+import sys
+
+# vars -----------------------------------------------------------------------
+mintWallet = Wallet()
+load_dotenv()
+price       = os.getenv('PRICE')
+#project     = str(os.getenv('PROJECT'))
+maxMint     = os.getenv('MAX_MINT')
+newPolicy   = False
+network     = TESTNET # NOTE CHANGE ON PRODUCTION
+
+
+project = None
+try:
+    project = sys.argv[1]
+    maxMint = sys.argv[2]
+except:
+    project = input("Please enter project name\n> ")
+    maxMint = input("Enter how many to generate\n> ")
+
+print(f"Project is {project}")
+
+apiManager = ApiManager(network=network, mint_wallet=mintWallet, project=project, nft_price_ada=price, max_mint=maxMint)
